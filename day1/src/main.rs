b4a188c3ebea0a790e36056422723efde8580754
Tom Rawcliffe <thomas.rawcliffe@skybettingandgaming.com>
2021-12-01 13:29:23 +0000

    Cleaner solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,40 +5,15 @@ fn main() {
         .lines()
         .filter_map(|l| u32::from_str_radix(&l.unwrap(), 10).ok())
         .collect();
-    println!("Part1: {}", part1(&input));
-    println!("Part2: {}", part2(&input, 3));
+    println!("Part1: {}", both_parts(&input, 1));
+    println!("Part2: {}", both_parts(&input, 3));
 }
 
-pub fn part1(input: &Vec<u32>) -> u32 {
-    let mut prev: Option<u32> = None;
-    let mut count = 0;
-
-    for current in input.iter() {
-        if let Some(p) = prev {
-            if p < *current {
-                count += 1;
-            }
-        }
-        prev = Some(*current);
-    }
-
-    count
-}
-
-pub fn part2(input: &Vec<u32>, window_size: usize) -> u32 {
-    let mut prev: Option<&[u32]> = None;
-    let mut count = 0;
-
-    for current in input.windows(window_size) {
-        if let Some(p) = prev {
-            if p.iter().sum::<u32>() < current.iter().sum() {
-                count += 1;
-            }
-        }
-        prev = Some(current);
-    }
-
-    count
+pub fn both_parts(input: &Vec<u32>, window_size: usize) -> u32 {
+    input
+        .windows(window_size + 1)
+        .filter(|v| v.iter().rev().skip(1).sum::<u32>() < v.iter().skip(1).sum())
+        .count() as u32
 }
 
 #[cfg(test)]
@@ -48,7 +23,7 @@ mod tests {
     #[test]
     fn test_part1() {
         let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-        assert_eq!(part1(&input), 7);
-        assert_eq!(part2(&input, 3), 5);
+        assert_eq!(both_parts(&input, 1), 7);
+        assert_eq!(both_parts(&input, 3), 5);
     }
 }
