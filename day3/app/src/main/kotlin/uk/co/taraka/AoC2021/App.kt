ea4be041da20e8202fc02e04ec2cbfe883864db8
Tom Rawcliffe <thomas.rawcliffe@skybettingandgaming.com>
2021-12-03 09:25:32 +0000

    bit of a tidy

--- day3/app/src/main/kotlin/uk/co/taraka/AoC2021/App.kt
+++ day3/app/src/main/kotlin/uk/co/taraka/AoC2021/App.kt
@@ -9,19 +9,18 @@ fun main() {
 }
 
 fun part1(input: List<String>): Int {
-    val counts: IntArray = input
+    val (γ, ε) = input
         .map { it.toList() }
         .fold(IntArray(input[0].length)) { acc, chars ->
             chars.forEachIndexed { idx, c -> if (c == '1') { acc[idx] += 1 } };
             acc  }
-
-    val (γ, ε) = counts.fold(Pair(0, 0)) { acc, it ->
-        if (it < input.size / 2) {
-            Pair(acc.first.shl(1) + 1, acc.second.shl(1))
-        } else {
-            Pair(acc.first.shl(1), acc.second.shl(1) + 1)
-        }
-    };
+        .fold(Pair(0, 0)) { acc, it ->
+            if (it < input.size / 2) {
+                Pair(acc.first.shl(1) + 1, acc.second.shl(1))
+            } else {
+                Pair(acc.first.shl(1), acc.second.shl(1) + 1)
+            }
+        };
 
     return γ * ε;
 }
