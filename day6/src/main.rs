82185da995416538e0cf8071e47c2ca638fe777c
Tom Rawcliffe <thomas.rawcliffe@skybettingandgaming.com>
2021-12-06 09:07:28 +0000

    Formatting changes

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -3,7 +3,10 @@ use std::io::{self, Read};
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
-    let input:Vec<usize> = buffer.split(",").filter_map(|x| usize::from_str_radix(x, 10).ok()).collect();
+    let input: Vec<usize> = buffer
+        .split(",")
+        .filter_map(|x| usize::from_str_radix(x, 10).ok())
+        .collect();
 
     println!("Part1: {}", run(&input, 80));
     println!("Part2: {}", run(&input, 256));
@@ -12,7 +15,7 @@ fn main() -> io::Result<()> {
 }
 
 fn create_tally(input: &Vec<usize>) -> [usize; 9] {
-    let mut tally = [0;9];
+    let mut tally = [0; 9];
     for x in input {
         tally[*x] += 1;
     }
@@ -20,17 +23,15 @@ fn create_tally(input: &Vec<usize>) -> [usize; 9] {
 }
 
 fn run(input: &Vec<usize>, num_days: usize) -> usize {
-    let mut tally = create_tally(input);
-
-    for i in 0..num_days {
-        tally[(i+7) % tally.len()] += tally[i % tally.len()];
-
-    }
-
-    tally.iter().sum()
+    (0..num_days)
+        .fold(create_tally(input), |tally, i| {
+            let mut t = tally.clone();
+            t[(i + 7) % tally.len()] += tally[i % tally.len()];
+            t
+        })
+        .iter()
+        .sum()
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -38,12 +39,9 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let input = vec![3,4,3,1,2];
+        let input = vec![3, 4, 3, 1, 2];
         assert_eq!(run(&input, 18), 26);
         assert_eq!(run(&input, 80), 5934);
         assert_eq!(run(&input, 256), 26984457539);
     }
 }
-
-
-
