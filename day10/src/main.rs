0c06733b4793c3b3c3242a75cfe76f7dd12327e4
Tom Rawcliffe <thomas.rawcliffe@skybettingandgaming.com>
2021-12-12 11:29:20 +0000

    Day10 part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,5 +1,4 @@
 #![feature(stdin_forwarders)]
-use std::collections::HashMap;
 use std::io::{self, Read};
 
 fn main() -> io::Result<()> {
@@ -15,36 +14,23 @@ fn main() -> io::Result<()> {
 fn part1(input: &str) -> usize {
     input
         .lines()
-        .filter_map(process_line)
+        .filter_map(process_line_part1)
         .sum()
 }
 
 fn part2(input: &str) -> usize {
-    input.lines().filter_map(process_line).sum()
+    let mut scores: Vec<usize> = input.lines().filter_map(process_line_part2).collect();
+    scores.sort();
+    scores[scores.len()/2]
 }
 
-fn process_line(line: &str) -> Option<usize> {
+fn process_line_part1(line: &str) -> Option<usize> {
     let mut stack = vec![];
     for c in line.chars() {
         match c {
             '(' | '[' | '<' | '{' => stack.push(c),
-            ')' => {
-                if stack.pop()? != '(' {
-                    return get_score(c);
-                }
-            }
-            ']' => {
-                if stack.pop()? != '[' {
-                    return get_score(c);
-                }
-            }
-            '>' => {
-                if stack.pop()? != '<' {
-                    return get_score(c);
-                }
-            }
-            '}' => {
-                if stack.pop()? != '{' {
+            ')' | ']' | '>' | '}' => {
+                if stack.pop()? != get_matching(c) {
                     return get_score(c);
                 }
             }
@@ -52,6 +38,50 @@ fn process_line(line: &str) -> Option<usize> {
         }
     }
     None
+}
+
+fn process_line_part2(line: &str) -> Option<usize> {
+    let mut stack = vec![];
+    for c in line.chars() {
+        match c {
+            '(' | '[' | '<' | '{' => stack.push(c),
+            ')' | ']' | '>' | '}' => {
+                if stack.pop()? != get_matching(c) {
+                    return None;
+                }
+            }
+            _ => panic!("Unknown char: {}", c)
+        }
+    }
+
+    Some(incomplete_score(&stack))
+}
+
+fn incomplete_score(stack: &Vec<char>) -> usize {
+    let mut total = 0;
+
+    for c in stack.iter().rev() {
+        total *= 5;
+        total += match c {
+            '(' => 1,
+            '[' => 2,
+            '{' => 3,
+            '<' => 4,
+            _ => panic!("Unknown char: {}", c)
+        }
+    }
+
+    total
+}
+
+fn get_matching(c: char) -> char {
+    match c {
+        ')' => '(',
+        ']' => '[',
+        '}' => '{',
+        '>' => '<',
+        _ =>  panic!("Unknown char: {}", c)
+    }
 }
 
 fn get_score(c: char) -> Option<usize> {
@@ -81,6 +111,6 @@ mod tests {
 <{([([[(<>()){}]>(<<{{
 <{([{{}}[<[[[<>{}]]]>[]]"#;
         assert_eq!(part1(&input), 26397);
-        // assert_eq!(part2(&input), 61229);
+        assert_eq!(part2(&input), 288957);
     }
 }
