7efb7a8b456dfe3e34908514304e20080d864b4e
Tom Rawcliffe <thomas.rawcliffe@skybettingandgaming.com>
2021-12-15 10:54:14 +0000

    Day15 day2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,33 +1,61 @@
-use itertools::Itertools;
-use std::io::{self, Read};
 use pathfinding::prelude::{absdiff, astar};
+use std::io::{self, Read};
 
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
 
-    println!("Part1: {}", run(&buffer));
-    // println!("Part2: {}", run(&buffer, 40));
+    println!("Part1: {}", run(&buffer, 1));
+    println!("Part2: {}", run(&buffer, 5));
 
     Ok(())
 }
 
-fn run(input: &str) -> u32 {
-    let map = parse_input(input);
-    let end = (map.len()-1, map[0].len()-1);
-    let route = astar(&(0,0), |p| neighbors(*p, &map), |(x, y)| (absdiff(end.0, *x) + absdiff(end.1, *y)) as u32, |p| *p == end).expect("No Route Found");
+fn run(input: &str, size: u32) -> u32 {
+    let map = parse_input(input, size);
+    let end = (map.len() - 1, map[0].len() - 1);
+    let route = astar(
+        &(0, 0),
+        |p| neighbors(*p, &map),
+        |(x, y)| (absdiff(end.0, *x) + absdiff(end.1, *y)) as u32,
+        |p| *p == end,
+    )
+    .expect("No Route Found");
     route.1
 }
 
 fn neighbors((x, y): (usize, usize), map: &Vec<Vec<u32>>) -> Vec<((usize, usize), u32)> {
-    let mut points = vec![(x+1, y), (x, y+1)];
-    if x > 0 { points.push((x-1, y)) }
-    if y > 0 { points.push((x, y-1)) }
-    points.iter().filter_map(|(px, py)| { Some(((*px, *py), *map.get(*px)?.get(*py)?)) } ).collect()
+    let mut points = vec![(x + 1, y), (x, y + 1)];
+    if x > 0 {
+        points.push((x - 1, y))
+    }
+    if y > 0 {
+        points.push((x, y - 1))
+    }
+    points
+        .iter()
+        .filter_map(|(px, py)| Some(((*px, *py), *map.get(*px)?.get(*py)?)))
+        .collect()
 }
 
-fn parse_input(input: &str) -> Vec<Vec<u32>> {
-    input.lines().map(|l| l.chars().filter_map(|c| c.to_digit(10)).collect()).collect()
+fn parse_input(input: &str, size: u32) -> Vec<Vec<u32>> {
+    (0..size)
+        .flat_map(|i| {
+            input
+                .lines()
+                .map(|l| {
+                    (0..size)
+                        .flat_map(|j| {
+                            l.chars()
+                                .filter_map(|c| c.to_digit(10))
+                                .map(|d| ((d + i + j - 1) % 9) + 1)
+                                .collect::<Vec<u32>>()
+                        })
+                        .collect()
+                })
+                .collect::<Vec<Vec<u32>>>()
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -46,7 +74,7 @@ mod tests {
 3125421639
 1293138521
 2311944581"#;
-        assert_eq!(run(&input1), 40);
-        // assert_eq!(run(&input1, 40), 2188189693529);
+        assert_eq!(run(&input1, 1), 40);
+        assert_eq!(run(&input1, 5), 315);
     }
 }
